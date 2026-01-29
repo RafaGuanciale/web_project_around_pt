09a991b10172443b6e344714978e7b2027e5314f
Rafael <rafaelguanciale@hotmail.com>
2026-01-29 16:34:07 -0300

    feat - Projeto finalizado para avaliação

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# Tripleten web_project_around_pt
+# TripleTen — Around the U.S.
+
+**visualizar projeto:** https://rafaguanciale.github.io/web_project_around_pt/
+
+Este projeto tem como objetivo tornar uma página totalmente dinâmica utilizando JavaScript, substituindo elementos HTML estáticos por componentes criados via código. A aplicação permite:
+- Criar, curtir, excluir e visualizar cards de forma interativa.
+
+Além disso, o projeto trabalha intensivamente com:
+- Manipulação do DOM
+- Eventos
+- Templates 
+- Formulários
+
+O foco principal desse projeto referente ao Sprint 8 do curso de Web Development da TripleTen, é consolidar conceitos fundamentais de JavaScript aplicados ao DOM, como criação dinâmica de elementos, reutilização de componentes, manipulação de eventos e organização do código em funções bem definidas.
+
+## Tecnologias Utilizadas
+
+- HTML5
+- CSS3
+- JavaScript (ES6+)
+
+## Principais Funcionalidades e Soluções Técnicas
+
+1. Função getCardElement()
+
+- Clona o template do card
+- Preenche título, imagem e texto alternativo
+- Adiciona eventos de curtir, excluir e abrir imagem
+- Utiliza parâmetros padrão para lidar com dados incompletos
+
+2. Função renderCard()
+
+- Responsável por inserir o card no DOM
+- Insere novos cards sempre no início do container
+- Mantém separação clara entre criação e renderização
+
+3. Cards iniciais
+
+- Os cards iniciais são renderizados a partir de um array de objetos
+- Um forEach() percorre os dados e chama renderCard(), criando os cards iniciais
+
+4. Adição de novos cards via formulário
+
+- Um pop-up permite criar novos cards dinamicamente.
+- Os cards são criados a partir da tag <template> do HTML
+- O formulário captura nome e link da imagem.
+- Ao enviar: 
+O comportamento padrão é prevenido.
+Um novo card é criado e inserido no DOM.
+O modal é fechado e o formulário resetado.
+
+5. Curtir e excluir cards
+
+- Cada card possui um botão de curtir e outro de excluir
+- O estado do botão é controlado via classList.toggle() e element.remove() respectivamente
+- O evento é adicionado no momento da criação do card.
+
+6. Controle de modais
+
+- Funções genéricas para abrir e fechar modais.
+- Modais são fechados por botão de fechar.
+
+## Aprendizados Pessoais
+
+Este foi um projeto no qual pude aplicar conceitos utilizados em aplicações reais, implementando interações comuns em sites modernos e tornando a página totalmente dinâmica para o usuário.
+Alguns aprendizados que consolidaram minha base:
+
+- Uso correto da tag <template> para criação dinâmica de componentes.
+- Separação de responsabilidades entre funções.
+- Manipulação eficiente do DOM.
+- Uso de eventos em elementos criados dinamicamente.
+- Diferença entre elementos, atributos e valores.
+- Importância de parâmetros padrão em funções.
+- Boas práticas para formulários e modais.
+
+## Melhorias Futuras
+
+- Fechar modais ao clicar no overlay.
+- Fechar modais ao pressionar a tecla ESC.
+- Adicionar validações mais avançadas nos formulários.
+- Implementar animações de abertura e fechamento dos modais.
+- Persistência dos cards utilizando armazenamento local (LocalStorage).
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const initialCards = [
   {
-    name: "Vale de Yosemit",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
   },
   {
     name: "Lago Louise",
@@ -25,42 +25,65 @@ const initialCards = [
   },
 ];
 
-initialCards.forEach(function(item) {
-    console.log(item.name);
-});
-
-const editPopupModal = document.querySelector("#edit-popup")
+const editPopupModal = document.querySelector("#edit-popup");
 const editProfileBtn = document.querySelector(".profile__edit-button");
-const closeEditProfileModal = editPopupModal.querySelector(".popup__close")
+const closeEditProfileModal = editPopupModal.querySelector(".popup__close");
 const formElement = editPopupModal.querySelector("#edit-profile-form");
 const profileName = document.querySelector(".profile__title");
 const profileJob = document.querySelector(".profile__description");
 const nameInput = editPopupModal.querySelector(".popup__input_type_name");
 const jobInput = editPopupModal.querySelector(".popup__input_type_description");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
+const newCardModal = document.querySelector("#new-card-popup");
+const newCardBtn = document.querySelector(".profile__add-button");
+const closeNewCardModal = newCardModal.querySelector(".popup__close");
+const newCardForm = newCardModal.querySelector("#new-card-form");
+const cardNameInput = newCardForm.querySelector(".popup__input_type_card-name");
+const cardLinkInput = newCardForm.querySelector(".popup__input_type_url");
+const imagePopupModal = document.querySelector("#image-popup");
+const closeImagePopupModal = imagePopupModal.querySelector(".popup__close");
+const cardImagePopup = imagePopupModal.querySelector(".popup__image");
+const cardPopupCaption = imagePopupModal.querySelector(".popup__caption");
 
-function openModal(modal){
+editProfileBtn.addEventListener("click", handleOpenEditModal);
+newCardBtn.addEventListener("click", () => {
+  openModal(newCardModal);
+});
+
+closeNewCardModal.addEventListener("click", () => {
+  closeModal(newCardModal);
+});
+
+closeImagePopupModal.addEventListener("click", () => {
+  closeModal(imagePopupModal);
+});
+
+function openModal(modal) {
   modal.classList.add("popup_is-opened");
 }
 
-function closeModal(modal){
+function closeModal(modal) {
   modal.classList.remove("popup_is-opened");
 }
 
-editProfileBtn.addEventListener("click",handleOpenEditModal);
-
-closeEditProfileModal.addEventListener("click", () =>{
-  closeModal(editPopupModal);
-})
-
-function fillProfileForm(){
+function fillProfileForm() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function handleOpenEditModal(){
+function handleOpenEditModal() {
   openModal(editPopupModal);
   fillProfileForm();
 }
+
+closeEditProfileModal.addEventListener("click", () => {
+  closeModal(editPopupModal);
+});
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -69,4 +92,53 @@ function handleProfileFormSubmit(evt) {
   closeModal(editPopupModal);
 }
 
-formElement.addEventListener("submit",handleProfileFormSubmit);
+function getCardElement(
+  name = "Lugar sem nome",
+  link = "images/placeholder.jpg",
+) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardLikeBtn = cardElement.querySelector(".card__like-button");
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-button");
+  cardTitle.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  cardLikeBtn.addEventListener("click", () => {
+    cardLikeBtn.classList.toggle("card__like-button_is-active");
+  });
+
+  cardDeleteBtn.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener("click", () => {
+    cardImagePopup.src = cardImage.src;
+    cardImagePopup.alt = name;
+    cardPopupCaption.textContent = name;
+    openModal(imagePopupModal);
+  });
+
+  return cardElement;
+}
+
+function renderCard(name, link, box) {
+  const cardCreated = getCardElement(name, link);
+  box.prepend(cardCreated);
+}
+
+initialCards.forEach((card) => {
+  renderCard(card.name, card.link, cardList);
+});
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  const name = cardNameInput.value;
+  const link = cardLinkInput.value;
+  renderCard(name, link, cardList);
+  closeModal(newCardModal);
+  evt.target.reset();
+}
+
+newCardForm.addEventListener("submit", handleCardFormSubmit);
